d801b7877d33dc34149efd9ab79975f494a41796
rodrigofrancisco <rodrigo.francisco@wizeline.com>
2022-11-30 11:51:39 -0600

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Rodrigo Francisco on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
